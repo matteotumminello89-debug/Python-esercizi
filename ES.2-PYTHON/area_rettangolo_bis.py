868ad6f1ccdb6f7fdd84f92f1c004d27bfb1cf83
Matteo <matteotumminello89@gmail.com>
2026-02-22 22:52:50 +0100

    Aggiunto Esercizio 2

---
+++ ES.2-PYTHON/area_rettangolo_bis.py
@@ -0,0 +1,7 @@
+def area_rettangolo():
+    base = float(input("Inserisci la base del rettangolo: "))
+    altezza = float(input("Inserisci l'altezza del rettangolo: "))
+    area = base * altezza
+    print("L'area del rettangolo è:", area)
+
+area_rettangolo()
