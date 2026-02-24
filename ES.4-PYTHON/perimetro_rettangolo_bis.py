ae766ce1e1e8138142f8be36790f1eaed5ba397d
Matteo <matteotumminello89@gmail.com>
2026-02-24 21:52:39 +0100

    Aggiunto Esercizio 4

---
+++ ES.4-PYTHON/perimetro_rettangolo_bis.py
@@ -0,0 +1,6 @@
+def perimetro_rettangolo():
+    base = float(input("Inserisci la base: "))
+    altezza = float(input("Inserisci l'altezza: "))
+    perimetro = 2 * (base + altezza)
+    print("Il perimetro è:", perimetro)
+perimetro_rettangolo()
