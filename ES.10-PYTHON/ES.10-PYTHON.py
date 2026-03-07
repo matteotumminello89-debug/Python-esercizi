41021b5fc22ea220644c8858ef64847c3f9e8909
Matteo <matteotumminello89@gmail.com>
2026-03-08 00:10:43 +0100

    Aggiunto Esercizio 9

---
+++ ES.10-PYTHON/ES.10-PYTHON.py
@@ -0,0 +1,21 @@
+def trova_minore_manuale():
+    numeri = []
+
+    # Chiedo all'utente di inserire 10 numeri interi
+    for i in range(10):
+        numero = int(input(f"Inserisci il numero {i+1}: "))
+        numeri.append(numero)
+
+    # Stampo i numeri inseriti
+    print("Numeri inseriti:", numeri)
+
+    # Trovo il valore minimo
+    minore = numeri[0]
+    for n in numeri:
+        if n < minore:
+            minore = n
+
+    return minore
+
+# Esempio di utilizzo
+print("Il numero minore è:", trova_minore_manuale())
---
+++ ES.9-PYTHON/ES.9-PYTHON.py
@@ -0,0 +1,23 @@
+import random
+
+def trova_minore():
+    # Genero una lista di 10 numeri interi casuali tra 1 e 100
+    numeri = []
+    for _ in range(10):
+        numero = random.randint(1, 100)
+        numeri.append(numero)
+
+    # Stampo i numeri generati
+    print("Numeri generati:", numeri)
+
+    # Trovo il valore minimo
+    minore = numeri[0]  # parto dal primo elemento
+    for n in numeri:
+        if n < minore:
+            minore = n
+
+    # Restituisco il minore
+    return minore
+
+# Esempio di utilizzo
+print("Il numero minore è:", trova_minore())
