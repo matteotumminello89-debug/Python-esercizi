ad365c05aac390bb323f92570ed464357fdfbda1
Matteo <matteotumminello89@gmail.com>
2026-03-17 20:39:03 +0100

    aggiunto ES. 15

---
+++ ES.15-PYTHON/ES.15-PYTHON.py
@@ -0,0 +1,6 @@
+codice = input("Inserisci il codice fiscale: ")
+
+if len(codice) == 16:
+    print("Codice fiscale valido (lunghezza corretta).")
+else:
+    print("Codice fiscale NON valido: deve contenere esattamente 16 caratteri.")
